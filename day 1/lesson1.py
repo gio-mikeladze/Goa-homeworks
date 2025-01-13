862b75b280997e68a8b7ffc47e0072d68fccdef3
gio-mikeladze <mikeladzegio2@gmail.com>
2025-01-13 21:06:37 +0400

    second commit

--- day 1/lesson1.py
+++ day 1/lesson1.py
@@ -1,5 +1,10 @@
+a="my name is:"
+b="i am:"
+c="years old"
+d="my height is:"
+e="cm"
 name="giorgi"
 surname="mikeladze"
 age=13
 height=157.5
-print(name + " " + surname + " " + str(age) + " " + str(height))
+print(a+ " " + name + " " + surname + " " + b + " " + str(age)+ " " + c + " " + d + " " + str(height) + e)
