2fe5bfdb5b8c579fb9562d720660cc13b5d78370
gio-mikeladze <mikeladzegio2@gmail.com>
2025-02-17 20:59:20 +0400

    second commit

---
+++ level 003/homework(3).py
@@ -0,0 +1,6 @@
+item="coat"
+print(item)
+item="boots"
+print(item)
+item="t-shirt"
+print(item)
---
+++ level 003/homework(4).py
@@ -0,0 +1,6 @@
+number1=567
+number2=7278
+number3=8629
+number4=1754
+number5=6573
+print(number1+number2+number3+number4+number5)
