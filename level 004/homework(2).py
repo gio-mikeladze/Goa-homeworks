a9fef76a96adce999c9429b2b42d29160b9a980d
gio-mikeladze <mikeladzegio2@gmail.com>
2025-02-21 22:50:29 +0400

    second commit

---
+++ level 004/homework(2).py
@@ -0,0 +1,7 @@
+#შექმენით 3 ცვლადი სადაც შეინახავთ თქვენთვის სასურველ მონაცემს. type ფუნქციის გამოყენებით დაბეჭდეთ მათი ტიპი.
+name="Gio"
+age=13
+studyingat="Goa"
+print(type(name))
+print(type(age))
+print(type(studyingat))
---
+++ level 004/homework(4).py
@@ -0,0 +1,5 @@
+number1=199
+number2=123
+number3=203
+Arithmeticmean=(number1+number2+number3)//3
+print(Arithmeticmean)
