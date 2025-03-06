c4805799b2b386363fe4f831d48df9a30ef0f92f
gio-mikeladze <mikeladzegio2@gmail.com>
2025-03-06 21:27:51 +0400

    second commit

---
+++ level 008/classwork.py
@@ -0,0 +1,20 @@
+#1) 
+Name = "Nika" 
+naMe = "Luka"
+NAME = "Data"
+naME = "Vano"
+name = "Demetre"
+NaMe = "Jan"
+name = "Giorgi"
+
+#რამდენი ცვლადი შეიქმნება ამ კოდის მიხედვით?(აღწერეთ რატომ)
+#ამ კოდში შეიქმნება 6 ცვლადი naMe = "Luka" Name = "Nika" NAME = "Data" naMe="vano" NaMe="Jan" და name="Giorgi" case-sensitivity-ს მიხედვით ცვლადები რომლის
+#სახელებიც ერთნაირია 1 ცვლადად აღიქვამს ანუ ხდება რეინიცილიზაცია ხოლო ცვლადებს რომლის სახელებიც განსხვავებულია სხვადასხვა ცვლადად
+#ჩათვლის
+
+#2) შექმენით 5 ცვლადი, snake_case-ის გამოყენებით და დაბეჭდეთ
+studying_at="GOA"
+My_Name="GIO"
+My_age=13
+My_Mentors_name="NIKa"
+i_Love="GOA"
---
+++ level 008/homework(3).py
@@ -0,0 +1,10 @@
+#5) დაწერეთ პროგრამა, რომელიც გამოითვლის 5 რიცხვის საშუალო არითმეტიკულს, 
+#მიღებული რიცხვი შეინახეთ ცვლადში, აიყვანეთ კუბში(მესამე ხარისხში) და დაბეჭდეთ.
+number1=22
+number2=33
+number3=45
+number4=60
+number5=100
+Arithmetic_mean=(number1+number2+number3+number4+number5)//5
+final_number=Arithmetic_mean**3
+print(final_number)
