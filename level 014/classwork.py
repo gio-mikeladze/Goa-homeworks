3c33e1ceace32dc8c6c718efe29f2c6a619bf586
gio-mikeladze <mikeladzegio2@gmail.com>
2025-03-31 23:21:54 +0400

    second commit

---
+++ level 014/classwork.py
@@ -0,0 +1,28 @@
+#1) შეასრულეთ 3-3 მაგალითი თითოეულ შედარების ოპერატორზე
+nika="msvenieri xma"
+print(nika=="msvenieri xma")
+nika="best teacher"
+print(nika=="best teacher")
+print("Gio"=="gio")
+print(nika!="cudi xma")
+print(5!=4)
+GOA="best"
+print(GOA!="bad")
+print(8>5)
+print(3>5)
+print(523645>27364783246)
+print(54<65)
+print(346275<63746)
+print(12345<8450)
+print(5>=3)
+print(3>=2)
+print(1>=32)
+print(3<=2)
+print(5<=3)
+print(8<=7754)
+#2) შექმენით ცვლადი, სადაც შეინახავთ თქვენს სახელს. შემდეგ მომხმარებელს შემოატანინეთ თავისი სახელი და თუ თქვენი სახელი ემთხვევა მისას, დაბეჭდეთ True, სხვა შემთხვევაში False.
+name1="GIO"
+name2=input("Enter a name: ")
+print(name1==name2)
+if name1==name2: print("true")
+else:print("false")
