dd35f6930fbc2a649ec3b5ab9bcfd9c661994e6e
gio-mikeladze <mikeladzegio2@gmail.com>
2025-04-03 19:08:31 +0400

    second commit

---
+++ level 014/homework(3).py
@@ -0,0 +1,9 @@
+a="registration: "
+print(a)
+email=input("Enter your email: ")
+parol=input("Enter your parol: ")
+b="Authorization: "
+print(b)
+Email=input("Enter an email: ")
+Parol=input("Enter a parol: ")
+print(email==Email,parol==Parol)
---
+++ level 014/homework(4).py
@@ -0,0 +1,13 @@
+#logical operator საჭიროა მანქანებისთვის რთული სიტუაციების შესაფასებლად.
+print(True and True)
+print(True or False)
+print(True or True)
+a= True
+b= False
+print(a or b)
+print(False and False)
+print(False and True)
+print(False or False)
+c=True
+d=False
+print(c and d)
