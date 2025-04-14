68eb68c11fa064512fdc56579a3ec4c3f6037f03
gio-mikeladze <mikeladzegio2@gmail.com>
2025-04-14 22:17:45 +0400

    second commit

---
+++ level 018/homework(1).py
@@ -0,0 +1,5 @@
+age=int(input("Enter your age: "))
+your_name=input("Enter your name: ")
+my_name="gio"
+if my_name==your_name and age>=18: print("True")
+else:print(False)
---
+++ level 018/homework(2).py
@@ -0,0 +1,3 @@
+my_name="gio"
+your_name=input("Enter your name: ")
+if your_name==my_name :print("coincidence")
