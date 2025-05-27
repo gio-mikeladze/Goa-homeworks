2683c430c53a410a4c485cd19c03558c25a53d18
gio-mikeladze <mikeladzegio2@gmail.com>
2025-05-27 18:43:38 +0400

    second commit

---
+++ level 028/homework/homework(4).py
@@ -0,0 +1,7 @@
+num1=int(input("Enter a number: "))
+num2=int(input("Enter a number: "))
+num3=int(input("Enter a number: "))
+if num1>10 and num2>10 and num3>10:
+    print("All the numbers that are larger than ten")
+else:
+    print("Some numbers that are less than or equal to ten.")
